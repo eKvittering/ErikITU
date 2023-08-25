56e3053e0bd94a7cfb6d80c4ea049ca991e47f06
eKvittering <mib@beckit.dk>
2023-08-25 12:36:31 +0200

    This Should Work Now?

--- App.tsx
+++ App.tsx
@@ -79,7 +79,7 @@ function App(): JSX.Element {
           <Section title="Step One">
             Edit <Text style={styles.highlight}>App.tsx</Text> to change this
             screen and then come back to see your edits.
-            Hello you suck!!!!
+            Hello you suck!!!
           </Section>
           <Section title="See Your Changes">
             <ReloadInstructions />
